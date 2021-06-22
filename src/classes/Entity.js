680bf01c78e87c9d150a5e788b8269b1f91fa5c0
Nathan Luu <nateluu28@gmail.com>
2021-06-22 14:52:19 -0700

    Create banana bomb

--- src/classes/Entity.js
+++ src/classes/Entity.js
@@ -1,55 +1,8 @@
-
-const SCALE = 2;
 class Entity {
-
-
-  constructor(options) {
+  constructor(options){
     this.pos = options.pos;
-    this.height = 32;
-    this.width = 32;
-    this.scaledWidth = SCALE * this.width;
-    this.scaledHeight = SCALE * this.height;
-    this.key_dir = 2;
-
-    this.draw = this.draw.bind(this);
-    this.drawFrame = this.drawFrame.bind(this);
+    
   }
-  /*
-    Direction 0 = UP
-              1 = RIGHT
-              2 = DOWN 
-              3 = LEFT
-
-   */
-
-  move(ctx, direction, key_dir) {
-    this.pos[0] += 32 * direction[0];
-    this.pos[1] += 32 * direction[1];
-    this.key_dir = key_dir;
-    this.draw(ctx);
-  }
-
-  drawFrame(img, ctx, frameX, frameY, canvasX, canvasY) {
-    ctx.drawImage(img,
-                frameX * this.width, frameY * this.height, this.width, this.height,
-                canvasX, canvasY, this.scaledWidth, this.scaledHeight);
-  }
-
-  draw(ctx) {
-    let drawFrame = this.drawFrame;
-    let pos = this.pos;
-    let key_dir = this.key_dir;
-
-    let img = new Image();
-    img.src = 'https://tcrf.net/images/e/e9/NeoEarlyBomberman.gif';
-
-    img.onload = function () {
-      // drawFrame(img, ctx, 0, 2, pos[0], pos[1]);// init render
-      drawFrame(img, ctx, 0, key_dir, pos[0], pos[1]);
-
-    };
-  }
-
 }
 
 export default Entity;
--- src/index.js
+++ src/index.js
@@ -1,55 +1,121 @@
-import Entity from './classes/Entity';
+const SCALE = 1;
+const WIDTH = 32;
+const HEIGHT = 32;
+const SCALED_WIDTH = SCALE * WIDTH;
+const SCALED_HEIGHT = SCALE * HEIGHT;
+const CYCLE_LOOP = [0, 1, 0, 2];
+const FACING_DOWN = 2;
+const FACING_UP = 0;
+const FACING_LEFT = 3;
+const FACING_RIGHT = 1;
+const FRAME_LIMIT = 12;
+const MOVEMENT_SPEED = 1;
+
 
 document.addEventListener("DOMContentLoaded", () => {
   let canvas = document.getElementById('board-canvas');
-  if (!canvas.getContext){
-    console.log('canvas is not supported');
-  } 
-
-  canvas.width = 500;
-  canvas.height = 500;
   let ctx = canvas.getContext('2d');
+  let keyObj = {};
+  let currentDirection = FACING_DOWN;
+  let currentLoopIndex = 0;
+  let frameCount = 0;
+  let posX = 0;
+  let posY = 0;
+  let playerImg = new Image();
+  let bombImg = new Image();
+  let bombPressed = false;
+  let bombX = 0;
+  let bombY = 0;
 
 
-// function init() {
-//   window.requestAnimationFrame(step);
+  loadImage();
+  window.addEventListener('keydown', handleKeyDown);
+  window.addEventListener('keyup', handleKeyUp);
 
-// }
-// const cycleLoop = [0, 1, 0, 2];
-// let currentLoopIndex = 0;
-// let frameCount = 0;
-// let currentDirection = 2;
+  function loadImage() {
+    playerImg.src = 'https://tcrf.net/images/e/e9/NeoEarlyBomberman.gif';
+    playerImg.onload = function() {
+      window.requestAnimationFrame(gameLoop);
+    };
 
-// function step() {
-//   frameCount++;
-//   if (frameCount < 30) {
-//     window.requestAnimationFrame(step);
-//     return;
-//   }
-//   frameCount = 0;
-//   ctx.clearRect(0, 0, canvas.width, canvas.height);
-//   drawFrame(cycleLoop[currentLoopIndex], currentDirection, 0, 0);
-//   currentLoopIndex++;
-//   if (currentLoopIndex >= cycleLoop.length) {
-//     currentLoopIndex = 0;
-//   }
-  
-//   window.requestAnimationFrame(step);
-// }
-  let options = { pos: [1,1]}
-  let e1  = new Entity(options)
-  e1.draw(ctx);
-  key('a', function(){ e1.move(ctx, [-1, 0], 3); console.log(e1.pos) });
-  key('d', function(){ e1.move(ctx, [1, 0], 1); console.log(e1.pos) });
-  key('w', function(){ e1.move(ctx, [0, -1], 0); console.log(e1.pos) });
-  key('s', function(){ e1.move(ctx, [0, 1], 2); console.log(e1.pos) });
+    bombImg.src = 'https://data.whicdn.com/images/27491025/original.png';
+  }  
+// Event listeners that will handle key presses of movement.
+  function handleKeyDown(e) {
+      keyObj[e.key] = true;
+  }
+
+  function handleKeyUp(e) {
+      keyObj[e.key] = false;
+  }
+
+  function drawFrame(img, frameX, frameY, canvasX, canvasY) {
+    ctx.drawImage(img,
+                  frameX * WIDTH, frameY * HEIGHT, WIDTH, HEIGHT,
+                  canvasX, canvasY, SCALED_WIDTH, SCALED_HEIGHT);
+  }
 
 
+  function gameLoop() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    let hasMoved = false;
+
+    if (keyObj.w) {
+      movePlayer(0, -MOVEMENT_SPEED, FACING_UP);
+      hasMoved = true;
+    } else if (keyObj.s) {
+      movePlayer(0, MOVEMENT_SPEED, FACING_DOWN);
+      hasMoved = true;
+    }
+
+    if (keyObj.a) {
+      movePlayer(-MOVEMENT_SPEED, 0, FACING_LEFT);
+      hasMoved = true;
+    } else if (keyObj.d) {
+      movePlayer(MOVEMENT_SPEED, 0, FACING_RIGHT);
+      hasMoved = true;
+    }
+
+    if (keyObj[' '] && !bombPressed){
+      console.log('space');
+      bombPressed = true;
+      bombX = posX;
+      bombY = posY;
+      setInterval(function() { bombPressed = false; }, 1000);
+    }
+
+    if (hasMoved) {
+      frameCount++;
+      if (frameCount >= FRAME_LIMIT) {
+        frameCount = 0;
+        currentLoopIndex++;
+        if (currentLoopIndex >= CYCLE_LOOP.length) {
+          currentLoopIndex = 0;
+        }
+      }
+    }
+
+    if (!hasMoved) {
+      currentLoopIndex = 0;
+    }
+    if (bombPressed){
+      ctx.drawImage(bombImg, bombX, bombY, SCALED_WIDTH, SCALED_HEIGHT);
+    }
 
 
-})
+    drawFrame(playerImg, CYCLE_LOOP[currentLoopIndex], currentDirection, posX, posY);
+    window.requestAnimationFrame(gameLoop);
+  }
 
+  function movePlayer(deltaX, deltaY, direction) {
+    if (posX + deltaX > 0 && posX + SCALED_WIDTH + deltaX < canvas.width) {
+      posX += deltaX;
+    }
+    if (posY + deltaY > 0 && posY + SCALED_HEIGHT + deltaY < canvas.height) {
+      posY += deltaY;
+    }
+    currentDirection = direction;
+  }
 
-
-
-
+});
