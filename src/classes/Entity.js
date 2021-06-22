36258511bcbff02903e8768116246d30988b131b
Nathan Luu <nateluu28@gmail.com>
2021-06-21 19:52:50 -0700

    Add keystrokes to movement

--- src/classes/Entity.js
+++ src/classes/Entity.js
@@ -1,3 +1,14 @@
 class Entity {
-  
+  constructor(options){
+    this.pos = options.pos;
+  }
+
+  move(direction) {
+    this.pos[0] += 32 * direction[0];
+    this.pos[1] += 32 * direction[1];
+  }
+
+
 }
+
+export default Entity;
--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const scaledHeight = scale * height;
   
   let img = new Image();
-  img.src = 'https://github.com/nateluu28/BombermanJs/blob/main/src/assets/NeoEarlyBomberman.png';
+  img.src = 'https://tcrf.net/images/e/e9/NeoEarlyBomberman.gif';
   img.onload = function() {
     init();
   };
@@ -58,3 +58,17 @@ function step() {
 
 
 })
+
+
+
+
+import Entity from './classes/Entity';
+
+window.Entity = Entity;
+let options = { pos: [1,1]}
+let e1  = new Entity(options)
+
+key('a', function(){ e1.move([-1, 0]); console.log(e1.pos) });
+key('d', function(){ e1.move([1, 0]); console.log(e1.pos) });
+key('w', function(){ e1.move([0, -1]); console.log(e1.pos) });
+key('s', function(){ e1.move([0, 1]); console.log(e1.pos) });
